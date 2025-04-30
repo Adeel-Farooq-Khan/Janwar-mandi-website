4627f4e9598eaaf4c7f033f468f983816d738b4c
EshaArif-1234 <128717640+EshaArif-1234@users.noreply.github.com>
2025-04-30 19:27:14 +0500

    change the crud of cate

--- app/api/categories/get/route.js
+++ app/api/categories/get/route.js
@@ -1,8 +1,7 @@
-import { connectDB } from "@/utils/db";
-import Category from "@/models/Category";
-
+import { connectToDB } from "../../../../lib/mongodb";
+import Category from "../../../../models/Category";
 export async function GET() {
-  await connectDB();
+  await connectToDB();
   const categories = await Category.find({});
   return Response.json(categories);
 }
--- app/api/categories/post/route.js
+++ app/api/categories/post/route.js
@@ -1,16 +1,21 @@
-import { connectDB } from "@/utils/db";
-import Category from "@/models/Category";
+import { NextResponse } from 'next/server';
+import { connectToDB } from "../../../../lib/mongodb";
+import Category from "../../../../models/Category";
+import { verifyToken } from '../../../../lib/verifyToken';
 
 export async function POST(req) {
-  await connectDB();
-  const { name } = await req.json();
-
-  if (!name) return new Response("Category name required", { status: 400 });
-
   try {
+    verifyToken(req);
+    await connectToDB();
+    
+    const { name } = await req.json();
+    if (!name) {
+      return new NextResponse("Category name is required", { status: 400 });
+    }
+
     const category = await Category.create({ name });
-    return Response.json(category);
+    return NextResponse.json(category);
   } catch (error) {
-    return new Response(error.message, { status: 500 });
+    return new NextResponse(error.message, { status: 500 });
   }
 }
